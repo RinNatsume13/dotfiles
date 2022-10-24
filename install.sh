c5723c8ff4eadc753c0345f8d104ab37b931db08
Rin chan <115593465+RinNatsume13@users.noreply.github.com>
2022-10-23 23:22:14 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pacman -S nitrogen vicious rofi gvfs polkit-gnome lxappearance kvantum breeze-gtk breeze noto-fonts noto-fonts-cjk noto-fonts-emoji base-devel networkmanager network-manager-applet gst-plugin-pipewire  kpipewire pipewire  pipewire-alsa  pipewire-audio  pipewire-jack  pipewire-pulse  pavucontrol polkit-gnome neofetch
+
+mkdir ~/.config/awesome
+cd ~/.config/awesome && wget wget https://files.catbox.moe/msf60d.gz && tar -xf msf60d.gz
+
+reboot
